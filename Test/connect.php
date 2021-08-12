f0d104e9a86a23085a2962204089831dd45036dd
SayaliGhadage <68659608+SayaliGhadge52@users.noreply.github.com>
2021-08-12 20:07:08 +0530

    Add files via upload

---
+++ Test/connect.php
@@ -0,0 +1,23 @@
+<?php
+	$userName = $_POST['userName'];
+	$address = $_POST['address'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$number = $_POST['number'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','testnew');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(userName, address, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $userName, $address, $gender, $email, $password, $number);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
